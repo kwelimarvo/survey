4c97164491334e0bdf867dd672a8f20c337cb2a2
kwelimarvo <marvin@kwelicapital.com>
2022-06-23 11:05:40 +0300

    Vuex installed

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,24 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import {mapState} from 'vuex'
+
+export default {
+  components: {HelloWorld},
+
+  computed: {
+    ...mapState(['user'])
+  },
+}
 </script>
 
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+    <pre>{{user.data.name}}</pre>
+
   </div>
 </template>
 
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,7 +3,10 @@ import { createStore } from 'vuex';
 const store = createStore(
     {
         state: {
-            },
+            user: {
+                data: {name: 'Marvin'},
+            }    
+        },
 
         getters: {
             
