7defa2eddd252b4953c75d375c2b7ee592f608b6
kwelimarvo <marvin@kwelicapital.com>
2022-06-23 12:44:51 +0300

    Vue Router Configured

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,9 @@ import store from './store'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import router from './router'
 
 createApp(App)
 .use(store)
+.use(router)
 .mount('#app')
