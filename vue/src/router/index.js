631425e45c8773a8cdff532674233abd60080a2e
kwelimarvo <marvin@kwelicapital.com>
2022-06-26 20:46:15 +0300

    Default Layout Added

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,15 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Dashboard from '../views/Dashboard.vue'
+import Surveys from '../views/Surveys.vue'
 import Register from '../views/Register.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
 import Login from '../views/Login.vue'
 
 
 const routes = [
     {
         path: '/',
+        redirect: '/dashboard',
         name: 'Dashboard',
-        component: Dashboard
+        component: DefaultLayout
     }, 
     {
         path: '/login',
